1d890c0a106dc918e4bba601e2ea8a375db14b95
wdk <fredddray@gmail.com>
2024-07-10 03:19:59 +0800

    ,

--- js/main.js
+++ js/main.js
@@ -2,11 +2,43 @@ $(".products-list-slider").slick({
   infinite: true,
   slidesToShow: 5,
   variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: false,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        variableWidth: false,
+      },
+    },
+  ],
 });
 
 $(".posts__slider").slick({
   infinite: true,
   slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: false,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        variableWidth: false,
+      },
+    },
+  ],
 });
 
 $(".catalog__catalog-button").on("click", function () {
@@ -14,3 +46,10 @@ $(".catalog__catalog-button").on("click", function () {
     // Animation complete.
   });
 });
+
+document.getElementById("menuBurger").onclick = () => {
+  document.getElementById("adaptiveMenuContent").classList.toggle("none");
+  setTimeout(() => {
+    document.getElementById("adaptiveMenuContent").classList.toggle("visible");
+  }, 1000);
+};
